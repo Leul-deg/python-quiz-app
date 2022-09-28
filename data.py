1ab19b6cf9709c42e570da3c062dbc8614eb0232
Leul Degarege <leuldegarege11@gmail.com>
2022-09-28 10:12:09 +0300

    the final quiz project

---
+++ data.py
@@ -0,0 +1,31 @@
+question_data = [
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Gumbo is a stew that originated in Louisiana.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+                                       "question": "French is an official language in Canada.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "It is automatically considered entrapment in the United States if the police sell you "
+                 "illegal substances without revealing themselves.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Nutella is produced by the German company Ferrero.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+                                      "question": "&quot;Ananas&quot; is mostly used as the word for Pineapple"
+                                                  " in other languages.",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "Pluto is a planet.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "When you cry in space, your tears stick to your face.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+                                       "question": "One of Donald Trump&#039;s 2016 Presidential Campaign"
+                                                   " promises was to build a border wall between "
+                                                   "the United States and Mexico.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Scotland voted to become an independent country during the referendum from September 2014.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Vietnam&#039;s national flag is a red star in front of a yellow background.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}]
